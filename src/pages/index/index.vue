b97255881929d1fcb420428a1ca6ff3b57995c29
xumingquan <xumingquan@yidian-inc.com>
2018-11-04 00:55:39 +0800

    修改样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="first" v-if="!nickNameIsEmpty" style="height: 1196rpx; width:750rpx;">
-      <img src="../../../static/images/backgrand_1@1x.png" style="height: 1196rpx; width:750rpx;position:absolute;z-index:-11;top:-50rpx"/>
+    <div class="first" v-if="!nickNameIsEmpty">
+      <img src="../../../static/images/backgrand_1@1x.png" style="height: 1196rpx; width:100%;position:absolute;z-index:-11;"/>
       <div class="business-card">
         <div class="company-logo">
           <img src="../../../static/images/icon_logo@1x.png" style="height: 56rpx;width:229rpx;"/>
@@ -16,37 +16,39 @@
           <div class="email">{{userInfo.email}}</div>
           <div class="address">{{userInfo.address}}</div>
         </div>
-        <div class="user-handle">
-          <!-- <img src="../../../static/images/icon_txl@1x.png" style="width: 23rpx; height: 23rpx;"/> <span>导入到通讯录</span> -->
-        </div>
       </div>
       <div v-if="qrToken" class="qr-code">
         <img :src="qrToken" style="width:194rpx;height:194rpx;margin-left:278rpx;"/>
       </div>
+      <div class="down-arrow">
+        <div>
+          <img src="../../../static/images/icon_jt@1x.png" style="width:52rpx;height:25rpx;"/>
+        </div>
+      </div>
     </div>
-    <div class="second" style="width:750rpx;height: 1284rpx;">
-      <img src="../../../static/images/backgrand_2@1x.png" style="height: 1334rpx; width:750rpx;position:absolute;z-index:-11;top:-50rpx"/>
-      <div class="theme" >
-        <img src="../../../static/images/theme@1x.png" style="width:477rpx;height:277rpx;"/>
-      </div>
-      <!-- <div class="slogan">
-        <img src="../../../static/images/slogan@1x.png" style="width:272rpx;height:106rpx;"/>
-      </div> -->
-      <div class="go-to-official" @click="goToOfficial">
-        <p></p><span></span>
-      </div>
-      <div class="login-button" v-if="!nickName"> 
-        <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">一点员工登录</button>
+    <!-- <div class="user-handle">
+      <img src="../../../static/images/icon_txl@1x.png" style="width: 23rpx; height: 23rpx;"/> <span>导入到通讯录</span>
+    </div> -->
+    <div class="second">
+      <img src="../../../static/images/backgrand_2@1x.png" style="height: 1184rpx; width:100%;position:absolute;z-index:-11;"/>
+      <div class="second-main">
+        <div class="theme" >
+          <img src="../../../static/images/theme@1x.png" style="width:477rpx;height:277rpx;"/>
+        </div>
+        <div class="go-to-official" @click="goToOfficial">
+          <p></p><span></span>
+        </div>
+        <div class="login-button" v-if="!nickName"> 
+          <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">一点员工登录</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import MenuItem from '@/components/menuItem'
 export default {
   components: {
-    MenuItem
   },
   data () {
     return {
@@ -68,7 +70,7 @@ export default {
   },
   methods: {
     async getEmployeeInfo (fromShareNickName) { // 获取一点员工信息
-      var nickName = this.nickName || fromShareNickName || ''
+      var nickName = fromShareNickName || this.nickName || ''
       let userInfo = await this.$store.dispatch('getUserInfo', encodeURIComponent(nickName))
       if (userInfo.length === 1) {
         this.userInfo = userInfo[0]
@@ -120,7 +122,10 @@ export default {
     box-sizing: border-box;
     .first {
       position: relative;
-      box-sizing: border-box;
+      box-sizing: border-box; 
+      height: 1196rpx; 
+      width:750rpx;
+      overflow:hidden;
       .business-card {
         margin: 50rpx 50rpx 0rpx;
         box-sizing: border-box;
@@ -162,58 +167,75 @@ export default {
       .qr-code {
         margin-top: 40rpx;
       }
+      .down-arrow {
+        position: absolute;
+        bottom: 37rpx;
+        height: 23rpx;
+        width: 100%;
+        text-align: center;
+        >div {
+          display: inline-block;
+        }
+      }
     }
     .second {
       position: relative;
       box-sizing: border-box;
+      width: 750rpx;
+      height: 1184rpx;
       margin-top: 9rpx;
-      padding-top: 334rpx; 
-      .theme {
-        margin: 0 136rpx;
-        width: 750rpx;
-        height: 277rpx;
-      }
-      .slogan {
-        margin: 241rpx 239rpx 0;
-        width: 750rpx;
-        height: 106rpx;
-      }
-      .go-to-official {
-        background:#e90026;
-        margin: 85rpx 345rpx 0;
-        width: 23rpx;height: 23rpx;
-        border-radius:50%;
-        position: relative;
-        p {
-          position: absolute;
-          width: 23rpx;height: 23rpx;
-          border-radius:50%;
-          animation: myfirst 1.5s infinite;
-          background: rgba($color: #c9c9c9, $alpha: 0.15)
+      overflow: hidden;
+      .second-main {
+        padding-top: 344rpx; 
+        .theme {
+          margin: 0 136rpx;
+          width: 750rpx;
+          height: 277rpx;
         }
-        span {
-          position: absolute;
-          display:block;
-          width: 23rpx;height: 23rpx;
+        .go-to-official {
+          background:#e90026;
+          margin: 160rpx 345rpx 0;
+          width: 23rpx;
+          height: 23rpx;
           border-radius:50%;
-          animation: second 1.5s infinite;
-          background: rgba($color:#e6e6e6, $alpha:0.17);
+          position: relative;
+          p {
+            position: absolute;
+            width: 23rpx;height: 23rpx;
+            border-radius:50%;
+            animation: myfirst 1.5s infinite;
+            background: rgba($color: #c9c9c9, $alpha: 0.15)
+          }
+          span {
+            position: absolute;
+            display:block;
+            width: 23rpx;height: 23rpx;
+            border-radius:50%;
+            animation: second 1.5s infinite;
+            background: rgba($color:#e6e6e6, $alpha:0.17);
+          }
         }
-      }
-      .login-button {
-        margin-top: 200rpx;
-        button {
-          width: 50%;
-          background: #e90026;
-          color: #fff;
+        .login-button {
+          margin-top: 164rpx;
+          button {
+            width: 464rpx;
+            background:#f9f9f9;
+            color: #e90026;
+            border: 1px solid #e90026;
+            border-radius: 50rpx;
+            outline: transparent;
+          }
+          button::after {
+            border: 0;
+          } 
         }
       }
     }
   } 
   @keyframes myfirst{
-    100% {transform: scale(3);}
+    100% {transform: scale(5);}
   }
   @keyframes second{
-    100% {transform: scale(6);}
+    100% {transform: scale(10);}
   }
 </style>
